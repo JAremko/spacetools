1bc356b686dc5f758265d5c194222a6b1d9c1592
JAremko <w3techplayground@gmail.com>
2018-04-23 01:42:34 +0300

    [WIP] Clojure tools

--- src/spacedoc/core.clj
+++ src/spacedoc/core.clj
@@ -1,9 +1,7 @@
 (ns spacedoc.core
   (:gen-class)
-  (:use [clojure.walk :as w :only [postwalk]]
-        [clojure.set :as set :only [union]]
-        [clojure.string :only [lower-case]])
   (:require [clojure.spec.alpha :as s]
+            [clojure.string :refer [lower-case]]
             [spacedoc.data :as data]
             [spacedoc.util :as util]
             [spacedoc.conf :as conf]))
@@ -16,65 +14,15 @@
 
 (def doc-dir (clojure.java.io/file "emacs-tools/export/target"))
 
-(def file-paths-all
+(def spacedoc-file-paths
   (->> doc-dir
        (file-seq)
        (filter #(.isFile %))
        (map str)
-       (into #{})))
+       (filter #(.endsWith (lower-case %) ".edn"))
+       (vec)))
 
-(def file-paths-edn
-  (filter #(.endsWith (clojure.string/lower-case %) ".edn")
-          file-paths-all))
+(util/children-tags :body (apply util/read-spacedoc-files spacedoc-file-paths))
 
-(def edn-file-paths->edn (apply util/edn-fps->fp->edn file-paths-edn))
-
-(def node-tree-seq
-  (partial tree-seq :children :children))
-
-(defn node-children
-  [tag in-doc]
-  (->> in-doc
-       (node-tree-seq)
-       (filter #(= (:tag %) tag))
-       (map :children)
-       (flatten)))
-
-(defn node-children-tag-set
-  [tag in-doc]
-  (->> in-doc
-       (node-children tag)
-       (map :tag)
-       (into #{})))
-
-(defn node-children-tag-set-in-docs
-  [tag docs]
-  (apply set/union
-         (map #(node-children-tag-set tag %)
-              docs)))
-
-(defn first-fail
-  [mspec doc]
-  (or (when-let [c (:children doc)]
-        (reduce
-         (fn [_ v]
-           (when-let [fail (first-fail mspec v)]
-             (reduced fail)))
-         nil c))
-      (when-not (s/valid? mspec doc)
-        (s/explain-str mspec doc))))
-
-(defn explain-first
-  [spec mspec docs-map]
-  (doall (map #(when-not (s/valid? spec (val %))
-                 (println "Spec failed in:" (key %))
-                 (println (first-fail mspec (val %))))
-              docs-map)))
-
-;; (println (node-children-tag-set-in-docs
-;;           :headline
-;;           (map val edn-file-paths->edn)))
-
-(explain-first :spacedoc.data/root :spacedoc.data/node edn-file-paths->edn)
 
 (println "====== OK =========")
--- src/spacedoc/util.clj
+++ src/spacedoc/util.clj
@@ -1,67 +1,75 @@
 (ns spacedoc.util
   (:gen-class)
-  (:use [clojure.core.reducers :only [fold]]
-        [clojure.set :as set :only [union]]
-        [clojure.string :only [lower-case]])
-  (:require [clojure.spec.alpha :as s]
+  (:require [clojure.core.reducers :refer [fold]]
+            [clojure.set :as set :refer [union]]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]
+            [clojure.spec.alpha :as s]
             [spacedoc.data :as data]
             [spacedoc.conf :as conf]))
 
 
-(defn edn-fps->fp->edn
-  ([edn-file-path]
-   (try {edn-file-path
-         (read-string (slurp edn-file-path))}
-        (catch Exception e
-          (throw
-           (Exception.
-            (format
-             "\"%s\" During parsing of \"%s\" file"
-             e
-             edn-file-path))))))
-  ([edn-file-path & fs]
+(defn explain-deepest
+  "Validate each node in DOC using multi-spec MSPEC.
+  Nodes will be validated in `postwalk` order and only
+  the first invalidation will be reported.
+  The function returns `nil` If all nodes are valid."
+  [mspec doc]
+  (or (when-let [c (:children doc)]
+        (reduce
+         (fn [_ v]
+           (when-let [fail (explain-deepest mspec v)]
+             (reduced fail)))
+         nil c))
+      (when-not (s/valid? mspec doc)
+        (s/explain-str mspec doc))))
+
+
+(defn read-spacedoc-files
+  "Read Spacedoc END files"
+  ([file-path]
+   (io!
+    (try
+      (with-open [input (->> file-path
+                             (clojure.java.io/reader)
+                             (java.io.PushbackReader.))]
+        (let [objs (repeatedly (partial edn/read {:eof :fin} input))]
+          (if (= :fin (second objs))
+            (first objs)
+            (throw
+             (Exception.
+              "Spacdoc file should contain single top level form")))))
+      (catch Exception e
+        (throw
+         (Exception.
+          (format "\"%s\" During parsing of \"%s\" file" e file-path)))))))
+  ([file-path & fs]
    (fold
     conf/*n-threads*
-    (fn ([xs x] (merge xs x)) ([] {}))
-    (fn [xs x] (conj xs (edn-fps->fp->edn x)))
-    (conj fs edn-file-path))))
+    (fn
+      ([xs x] (merge xs x))
+      ([] '()))
+    #(conj %1 (read-spacedoc-files %2))
+    (conj fs file-path))))
 
 
-(defn foo [& files]
-  (println (first files)))
-
-
-(def node-tree-seq
-  (partial tree-seq :children :children))
-
-
-(defn node-children
-  [tag in-doc]
-  (->> in-doc
-       (node-tree-seq)
-       (filter #(= (:tag %) tag))
-       (map :children)
-       (flatten)))
-
-
-(defn node-children-tag-set
-  [tag in-doc]
-  (->> in-doc
-       (node-children tag)
-       (map :tag)
-       (into #{})))
-
-
-(defn node-children-tag-set-in-docs
-  [tag docs]
-  (apply set/union
-         (map #(node-children-tag-set tag %)
-              docs)))
-
-
-(defn explain-all
-  [spec docs-map]
-  (doall (map #(when-not (s/valid? spec (val %))
-                 (println "Spec failed in:" (key %))
-                 (s/explain spec (val %)))
-              docs-map)))
+(defn children-tags
+  "Return tag set of all children of a node specified by :tag value TAG
+  in IN-DOCS Spacemacs EDN documentation EDN structures."
+  ([tag]
+   (fn [xs]
+     (->> xs
+          (tree-seq :children :children)
+          (filter #(= (:tag %) tag))
+          (map :children)
+          (flatten)
+          (map :tag)
+          (into #{}))))
+  ([tag in-docs]
+   (fold
+    conf/*n-threads*
+    (fn
+      ([xs x] (union xs x))
+      ([] #{}))
+    #(union %1 ((children-tags tag) %2))
+    in-docs)))
