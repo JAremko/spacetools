1a14dd4fd2a4ea58dc121c210517b473e32742cf
JAremko <w3techplayground@gmail.com>
2018-09-07 20:16:44 +0300

    cleanup

--- clj_tools/spacedoc/test/spacedoc/data/node_test.clj
+++ clj_tools/spacedoc/test/spacedoc/data/node_test.clj
@@ -18,12 +18,10 @@
            (is (s/spec? f-spec#))))
        ;; Spec-test
        (when f-spec#
-         (binding [s/*recursion-limit* 2
-                   *assert* false]
-           (deftest ~(symbol (str f-name "-generates-valid-node"))
-             (let [fl# (->> (s/exercise-fn ~v 10)
-                            (map second)
-                            (keep #(s/abbrev (s/explain-data (:ret f-spec#) %)))
-                            first)]
-               (is (every? nil? fl#)
-                   (str "Validation fails: " fl#))))))))))
+         (deftest ~(symbol (str f-name "-generates-valid-node"))
+           (let [fl# (->> (s/exercise-fn ~v 10)
+                          (map second)
+                          (keep #(s/abbrev (s/explain-data (:ret f-spec#) %)))
+                          first)]
+             (is (every? nil? fl#)
+                 (str "Validation fails: " fl#)))))))))
