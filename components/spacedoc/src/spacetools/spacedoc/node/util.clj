abb8c1c406836a7cb71f229008d5c668bfefb628
JAremko <w3techplayground@gmail.com>
2018-12-27 02:42:01 +0200

    Spec node helpers

--- components/spacedoc/src/spacetools/spacedoc/node/util.clj
+++ components/spacedoc/src/spacetools/spacedoc/node/util.clj
@@ -1,19 +1,22 @@
 (ns spacetools.spacedoc.node.util
   "Helpers for writing specs and constructors for nodes."
   (:require [clojure.core.reducers :as r]
+            [clojure.spec.alpha :as s]
             [clojure.string :as str]
             [orchestra.core :refer [defn-spec]]
-            [spacetools.spacedoc.config :as cfg]))
+            [spacetools.spacedoc.config :as cfg]
+            [spacetools.spacedoc.core :as sc]))
 
 
-(defn headline?
-  [node]
-  (= (:tag node) :headline))
+(defn-spec headline? boolean?
+  "Return true if NODE is headline."
+  [x any?]
+  (= (:tag x) :headline))
 
 
-(defn headline->depth
+(defn-spec headline->depth pos-int?
   "return how deeply children of headline go."
-  [headline]
+  [headline headline?]
   ((fn rec [depth node]
      (if (headline? node)
        (inc (r/reduce (r/monoid max (constantly 0))
@@ -22,9 +25,9 @@
    0 headline))
 
 
-(defn clamp-headline-children
+(defn-spec clamp-headline-children headline?
   "delete hl headline children that are deeper than level."
-  [level headline]
+  [level pos-int? headline headline?]
   ((fn rec [depth {:keys [value children todo?] :as node}]
      (assoc node :children
             (if (and (headline? node) (>= depth level))
@@ -33,38 +36,42 @@
    1 headline))
 
 
-(defn mark-empty-as-todo
+(defn-spec mark-empty-as-todo headline?
   "mark headline as todo if i doesn't have children."
-  [headline]
+  [headline headline?]
   (if (empty? (:children headline))
     (assoc headline :todo? true)
     headline))
 
 
-(defn fmt-headline
+(defn-spec fmt-headline headline?
   "Used to fix generated headlines so they will pass spec checks."
-  [max-level headline]
+  [max-level pos-int? headline headline?]
   (->> headline (clamp-headline-children max-level) (mark-empty-as-todo)))
 
 
-(defn todo-or-has-children?
+(defn-spec todo-or-has-children? boolean?
   "HEADLINE node should have children or value of `:todo?` should be `true`."
-  [headline] (or (:todo? headline) (seq (:children headline))))
+  [headline headline?] (or (:todo? headline) (seq (:children headline))))
 
 
-(defn link->link-prefix
+(defn-spec link->link-prefix string?
   "given full link return corresponding prefix(usually protocol + ://)."
-  [path]
+  [path (s/and string? (complement str/blank?))]
   (->> (vals (cfg/link-type->prefix))
        (filter (partial str/starts-with? path))
        (first)))
 
 
-(defn list-item
+;; NOTE: Had to wrap output spec in `s/valid?` because we don't have
+;;       the spec yet (and `defn-spec` queries it).
+(defn-spec list-item #(s/valid? :spacetools.spacedoc.node/list-item %)
   "create list item node."
-  [item-type idx item]
+  [item-type #{:ordered :unordered}
+   idx nat-int?
+   item (s/+ :spacetools.spacedoc.node.item-children/child)]
   {:tag :list-item
    :type item-type
    :bullet (if (= item-type :unordered) "- " (str (inc idx) ". "))
    :checkbox nil
-   :children [{:tag :item-children :children item}]})
+   :children [{:tag :item-children :children (vec item)}]})
