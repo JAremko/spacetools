b6b1a52e9decaa6e559c03b0576ff549703bd5f8
JAremko <w3techplayground@gmail.com>
2019-04-18 04:42:25 +0300

    [spacedoc] namespace configs

--- components/spacedoc/src/spacetools/spacedoc/config.clj
+++ components/spacedoc/src/spacetools/spacedoc/config.clj
@@ -10,112 +10,124 @@
   "File name of configurations overrides file."
   "sdn_overrides.edn")
 
+
 (def default-config
   "Default configurations."
-  {:layers.org/valid-tags [{"chat" "Chats"}
-                           {"checker" "Checkers"}
-                           {"completion" "Completion"}
-                           {"distribution" "Distributions"}
-                           {"e-mail" "E-mail"}
-                           {"emacs" "Emacs"}
-                           {"framework" "Frameworks"}
-                           {"fun" "Fun"}
-                           {"i18n" "internationalization"}
-                           {"markup"}
-                           {"misc"}
-                           {"os"}
-                           {"pair programming"}
-                           {"lang"}
-                           {"extra"}
-                           {"dsl"}
-                           {"lisp"}
-                           {"script"}
-                           {"general"}
-                           {"Imperative"}
-                           {"multi-paradigm"}
-                           {"js"}
-                           {"functional"}
-                           {"versioning"}
-                           {"tagging"}
-                           {"theme"}
-                           {"tool"}
-                           {"vim"}
-                           {"web"}]}
-  :layers.org/structure
-  [{{"lang" "Programming language"}
-    ["functional" "Purely functional"]
-    }]
-  {:text/separators-rigth #{\space \! \' \( \tab \newline \, \. \‘ \: \; \{ \“ \\ \} \?}
-   :text/separators-left #{\space \! \' \tab \) \newline \, \. \’ \: \; \{ \\ \” \} \?}
-   :text/replacement-map {"\\r+" ""
-                          "\\t" " "
-                          "[ ]{2,}" " "
-                          ;; Key-binding
-                          "(?:(?i)(\\p{Blank}|\\p{Blank}\\p{Punct}+|^)(k){1}ey)(?:(?:(?i)[-_]*b)| B)(?:(?i)inding)((?i)s{0,1}(?:\\p{Blank}|\\p{Punct}+\\p{Blank}|\\p{Punct}+$|$))" "$1$2ey binding$3"
-                          "((?i)k)ey bindingS" "$1ey bindings"}
-   :link/custom-id-replacement-map {"(?i)([-]+|^|#)key(?:[_]*|-{2,})binding([s]{0,1})([-]+|$)" "$1key-binding$2$3"}
-   :link/type->prefix {:file "file:"
-                       :http "http://"
-                       :https "https://"
-                       :custom-id "#"
-                       :ftp "ftp://"}
-   :headline/max-depth 5
-   :org/toc-max-depth 4
-   :org/toc-template "Table of Contents                     :TOC_%s_gh:noexport:"
-   :org/emphasis-tokens {:bold "*"
-                         :italic "/"
-                         :verbatim "="
-                         :underline "_"
-                         :kbd "~"  ;; Called code in the "classic" org.
-                         :strike-through "+"}
-   :org/block-indentation 2
-   :org/table-indentation 0})
+  #::{:layers_org-valid-tags [{"chat" "Chats"}
+                              {"checker" "Checkers"}
+                              {"completion" "Completion"}
+                              {"distribution" "Distributions"}
+                              {"e-mail" "E-mail"}
+                              {"emacs" "Emacs"}
+                              {"framework" "Frameworks"}
+                              {"fun" "Fun"}
+                              {"i18n" "internationalization"}
+                              {"markup" "Markup languages"}
+                              {"misc" "Misc"}
+                              {"os" "Operating systems"}
+                              {"pairing" "Pair programming"}
+                              {"lang" "Programming languages"}
+                              {"extra" "Extra"}
+                              {"dsl" "Domain-specific"}
+                              {"lisp" "Lisp dialects"}
+                              {"script" "Scripting"}
+                              {"general" "General-purpose"}
+                              {"Imperative" "Imperative"}
+                              {"multi-paradigm" "Multi-paradigm"}
+                              {"js" "JavaScript"}
+                              {"pure" "Purely functional"}
+                              {"versioning" "Source control"}
+                              {"tagging" "Tags"}
+                              {"theme" "Themes"}
+                              {"tool" "Tools"}
+                              {"vim" "Vim"}
+                              {"web services" "Web services"}]
+      :layers_org-structure [{"lang" ["pure"
+                                      {"general" ["imperative" "multi-paradigm"]}]}
+                             "markup"]
+      :text-separators-rigth #{\space \! \' \( \tab \newline \, \. \‘ \: \; \{ \“ \\ \} \?}
+      :text-separators-left #{\space \! \' \tab \) \newline \, \. \’ \: \; \{ \\ \” \} \?}
+      :text-replacement-map {"\\r+" ""
+                             "\\t" " "
+                             "[ ]{2,}" " "
+                             ;; Key-binding
+                             "(?:(?i)(\\p{Blank}|\\p{Blank}\\p{Punct}+|^)(k){1}ey)(?:(?:(?i)[-_]*b)| B)(?:(?i)inding)((?i)s{0,1}(?:\\p{Blank}|\\p{Punct}+\\p{Blank}|\\p{Punct}+$|$))" "$1$2ey binding$3"
+                             "((?i)k)ey bindingS" "$1ey bindings"}
+      :link-custom-id-replacement-map {"(?i)([-]+|^|#)key(?:[_]*|-{2,})binding([s]{0,1})([-]+|$)" "$1key-binding$2$3"}
+      :link-type->prefix {:file "file:"
+                          :http "http://"
+                          :https "https://"
+                          :custom-id "#"
+                          :ftp "ftp://"}
+      :headline-max-depth 5
+      :org-toc-max-depth 4
+      :org-toc-template "Table of Contents                     :TOC_%s_gh:noexport:"
+      :org-emphasis-tokens {:bold "*"
+                            :italic "/"
+                            :verbatim "="
+                            :underline "_"
+                            :kbd "~"  ;; Called code in the "classic" org.
+                            :strike-through "+"}
+      :org-block-indentation 2
+      :org-table-indentation 0})
 
-(s/def :text/separators-rigth (s/coll-of char? :kind set?))
 
-(s/def :text/separators-left (s/coll-of char? :kind set?))
+;; (s/def :layers_org/valid-tags (s/coll-of (s/map-of string? string?)
+;;                                          :kind vector?))
 
-(s/def :text/replacement-map (s/map-of string? string?))
+;; (s/def :layers_org.structure )
 
-(s/def :link/custom-id-replacement-map (s/map-of string? string?))
+;;   (s/def :layers_org/structure (s/coll-of
+;;                                 (s/or :join :layers_org.structure/join
+;;                                       :select :layers_org.structure/select)
+;;                                 :kind vector?))
 
-(s/def :link/type->prefix (s/map-of keyword? string?))
 
-(s/def :headline/max-depth nat-int?)
+(s/def ::text-separators-rigth (s/coll-of char? :kind set?))
 
-(s/def :org/toc-max-depth nat-int?)
+(s/def ::text-separators-left (s/coll-of char? :kind set?))
 
-(s/def :org/toc-template (s/and string? #(re-matches #".*%s.*" %)))
+(s/def ::text-replacement-map (s/map-of string? string?))
 
-(s/def :org/emphasis-tokens (s/map-of keyword? string?))
+(s/def ::link-custom-id-replacement-map (s/map-of string? string?))
 
-(s/def :org/block-indentation nat-int?)
+(s/def ::link-type->prefix (s/map-of keyword? string?))
 
-(s/def :org/table-indentation nat-int?)
+(s/def ::headline-max-depth nat-int?)
 
-(s/def ::configs (s/keys :req [:text/separators-rigth
-                               :text/separators-left
-                               :text/replacement-map
-                               :link/custom-id-replacement-map
-                               :link/type->prefix
-                               :headline/max-depth
-                               :org/toc-max-depth
-                               :org/toc-template
-                               :org/emphasis-tokens
-                               :org/block-indentation
-                               :org/table-indentation]))
+(s/def ::org-toc-max-depth nat-int?)
 
-(s/def ::overriding-configs (s/keys :op [:text/separators-rigth
-                                         :text/separators-left
-                                         :text/replacement-map
-                                         :link/custom-id-replacement-map
-                                         :link/type->prefix
-                                         :headline/max-depth
-                                         :org/toc-max-depth
-                                         :org/toc-template
-                                         :org/emphasis-tokens
-                                         :org/block-indentation
-                                         :org/table-indentation]))
+(s/def ::org-toc-template (s/and string? #(re-matches #".*%s.*" %)))
+
+(s/def ::org-emphasis-tokens (s/map-of keyword? string?))
+
+(s/def ::org-block-indentation nat-int?)
+
+(s/def ::org-table-indentation nat-int?)
+
+(s/def ::configs (s/keys :req [::text-separators-rigth
+                               ::text-separators-left
+                               ::text-replacement-map
+                               ::link-custom-id-replacement-map
+                               ::link-type->prefix
+                               ::headline-max-depth
+                               ::org-toc-max-depth
+                               ::org-toc-template
+                               ::org-emphasis-tokens
+                               ::org-block-indentation
+                               ::org-table-indentation]))
+
+(s/def ::overriding-configs (s/keys :op [::text-separators-rigth
+                                         ::text-separators-left
+                                         ::text-replacement-map
+                                         ::link-custom-id-replacement-map
+                                         ::link-type->prefix
+                                         ::headline-max-depth
+                                         ::org-toc-max-depth
+                                         ::org-toc-template
+                                         ::org-emphasis-tokens
+                                         ::org-block-indentation
+                                         ::org-table-indentation]))
 
 
 (def *configs
@@ -144,16 +156,16 @@ Same as `valid-configs?` but all elements of the CONFIGS map are optional."
 
 ;;;; General
 
-(defn-spec seps-right :text/separators-rigth
+(defn-spec seps-right ::text-separators-rigth
   "Return right separators."
   []
-  (:text/separators-rigth @*configs))
+  (::text-separators-rigth @*configs))
 
 
-(defn-spec seps-left :text/separators-left
+(defn-spec seps-left ::text-separators-left
   "Return left separators."
   []
-  (:text/separators-left @*configs))
+  (::text-separators-left @*configs))
 
 
 (defn-spec text-rep-map (s/map-of regexp? string?)
@@ -161,7 +173,7 @@ Same as `valid-configs?` but all elements of the CONFIGS map are optional."
   []
   (reduce-kv (fn [m k v] (assoc m (re-pattern k) v))
              {}
-             (:text/replacement-map @*configs)))
+             (::text-replacement-map @*configs)))
 
 
 (defn-spec custom-id-link-rep-map (s/map-of regexp? string?)
@@ -169,13 +181,13 @@ Same as `valid-configs?` but all elements of the CONFIGS map are optional."
   []
   (reduce-kv (fn [m k v] (assoc m (re-pattern k) v))
              {}
-             (:link/custom-id-replacement-map @*configs)))
+             (::link-custom-id-replacement-map @*configs)))
 
 
-(defn-spec link-type->prefix :link/type->prefix
+(defn-spec link-type->prefix ::link-type->prefix
   "Given link type return corresponding prefix."
   []
-  (:link/type->prefix @*configs))
+  (::link-type->prefix @*configs))
 
 
 (defn-spec link-types (s/coll-of keyword? :kind set?)
@@ -184,39 +196,39 @@ Same as `valid-configs?` but all elements of the CONFIGS map are optional."
   (-> (link-type->prefix) keys set))
 
 
-(defn-spec max-headline-depth :headline/max-depth
+(defn-spec max-headline-depth ::headline-max-depth
   "Return max depth(level) that headline can have."
   []
-  (:headline/max-depth @*configs))
+  (::headline-max-depth @*configs))
 
 
-(defn-spec toc-max-depth :org/toc-max-depth
+(defn-spec toc-max-depth ::org-toc-max-depth
   "Return depth after which TOC entries cut-off."
   []
-  (:org/toc-max-depth @*configs))
+  (::org-toc-max-depth @*configs))
 
 
 (defn-spec toc-hl-val string?
   "Return standard headline for a TOC."
   []
-  (format (:org/toc-template @*configs) (toc-max-depth)))
+  (format (::org-toc-template @*configs) (toc-max-depth)))
 
 
 ;;;; Org
 
-(defn-spec emphasis-tokens :org/emphasis-tokens
+(defn-spec emphasis-tokens ::org-emphasis-tokens
   "Return emphasis tokens of org-mode."
   []
-  (:org/emphasis-tokens @*configs))
+  (::org-emphasis-tokens @*configs))
 
 
-(defn-spec begin-end-indentation :org/block-indentation
+(defn-spec begin-end-indentation ::org-block-indentation
   "Return indentation of org-mode BEGIN-END blocks."
   []
-  (:org/block-indentation @*configs))
+  (::org-block-indentation @*configs))
 
 
-(defn-spec table-indentation :org/table-indentation
+(defn-spec table-indentation ::org-table-indentation
   "Return indentation of org-mode tables."
   []
-  (:org/table-indentation @*configs))
+  (::org-table-indentation @*configs))
