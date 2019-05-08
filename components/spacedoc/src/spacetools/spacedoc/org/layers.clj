221f4761865323924370df20e5ac733b6692d7df
JAremko <w3techplayground@gmail.com>
2019-05-08 10:40:01 +0300

    [layers] make entries unique

--- components/spacedoc/src/spacetools/spacedoc/org/layers.clj
+++ components/spacedoc/src/spacetools/spacedoc/org/layers.clj
@@ -1,6 +1,7 @@
 (ns spacetools.spacedoc.org.layers
   "layers.org generator."
-  (:require [clojure.spec.alpha :as s]
+  (:require [clojure.set :refer [difference]]
+            [clojure.spec.alpha :as s]
             [clojure.string :as str]
             [orchestra.core :refer [defn-spec]]
             [spacetools.fs-io.interface :as io]
@@ -39,6 +40,43 @@
                vector)))))
 
 
+(defn rm-file-prefix [path]
+  (str/replace path #"^file:" ""))
+
+
+(defn add-file-prefix [path]
+  (str "file:" path))
+
+
+(defn relativize [path old-root other]
+  (io/relativize path (io/join (io/parent old-root) other)))
+
+
+(defn re-root-sdn [root-dir path sdn]
+  (assoc sdn
+         :source
+         (-> root-dir
+             (relativize path (rm-file-prefix path))
+             (str/replace #"(?ix)\.sdn$" ".org"))
+         :root-dir root-dir))
+
+
+(defn fix-relative-links [root-dir path doc]
+  ((fn inner [f-p sdn]
+     (if (s/valid? :spacetools.spacedoc.node/link sdn)
+       (condp = (:type sdn)
+         :file (update sdn :path #(->> %
+                                       rm-file-prefix
+                                       (relativize root-dir f-p)
+                                       add-file-prefix))
+         :custom-id  (assoc sdn
+                            :path (add-file-prefix (:source doc))
+                            :type :file)
+         sdn)
+       (update sdn :children (partial mapv (partial inner path)))))
+   path doc))
+
+
 ;; TODO Replace TAG validation with SPEC on configs read.
 ;; TODO Handle case when the root node ends up empty.
 (defn-spec layers-sdn (s/nilable :spacetools.spacedoc.node/root)
@@ -47,64 +85,41 @@ ROOT-DIR is the directory that will be used to resolve relative paths against.
 In PATH->SDN map PATH(keys) are original file paths and SDN(values) are docs."
   [root-dir io/file-ref?
    path->sdn (s/map-of io/file-ref? :spacetools.spacedoc.node/root)]
-  (letfn [(walk [ds node]
-            (let [tag (if (map? node)
-                        (ffirst node)
-                        node)]
-              (if-not ((cfg/valid-tags) tag)
-                (throw (ex-info "Query has invalid tag" {:tag tag}))
-                (when-let [f-ds (seq (filter #((:tags %) tag) ds))]
-                  (apply n/headline (or ((cfg/valid-tags) tag)
-                                        (->> tag
-                                             (format "<\"%s\" invalid tag>")
-                                             str/upper-case))
-                         (if (string? node)
-                           (mapv #(describe %) f-ds)
-                           (->> node
-                                first
-                                val
-                                (map (partial walk f-ds))
-                                (remove nil?))))))))
-
-          (rm-file-prefix [path]
-            (str/replace path #"^file:" ""))
-
-          (add-file-prefix [path]
-            (str "file:" path))
-
-          (relativize [path old-root other]
-            (io/relativize path (io/join (io/parent old-root) other)))
-
-          (re-root-sdn [path sdn]
-            (assoc sdn
-                   :source
-                   (-> root-dir
-                       (relativize path (rm-file-prefix path))
-                       (str/replace #"(?ix)\.sdn$" ".org"))
-                   :root-dir root-dir))
-
-          (fix-relative-links [path doc]
-            ((fn inner [f-p sdn]
-               (if (s/valid? :spacetools.spacedoc.node/link sdn)
-                 (condp = (:type sdn)
-                   :file (update sdn :path #(->> %
-                                                 rm-file-prefix
-                                                 (relativize root-dir f-p)
-                                                 add-file-prefix))
-                   :custom-id  (assoc sdn
-                                      :path (add-file-prefix (:source doc))
-                                      :type :file)
-                   sdn)
-                 (update sdn :children (partial mapv (partial inner path)))))
-             path doc))]
-
-    (some->> (cfg/layers-org-query)
-             seq
-             (hash-map "layer")
-             (walk (pmap #(->> %
-                               (apply re-root-sdn)
-                               (fix-relative-links (first %)))
-                         path->sdn))
-             :children
-             seq
-             (apply n/root "Configuration layers" #{}))))
+  (let [all-docs-v (volatile! #{})
+        walk (fn inner [ds node]
+               (let [tag (if (map? node)
+                           (ffirst node)
+                           node)]
+                 (if-not ((cfg/valid-tags) tag)
+                   (throw (ex-info "Query has invalid tag" {:tag tag}))
+                   (when-let [f-ds (seq (filter #(and ((:tags %) tag)
+                                                      (@all-docs-v %))
+                                                ds))]
+                     (apply n/headline (or ((cfg/valid-tags) tag)
+                                           (->> tag
+                                                (format "<\"%s\" invalid tag>")
+                                                str/upper-case))
+                            (if (map? node)
+                              (->> node
+                                   first
+                                   val
+                                   (map (partial inner f-ds))
+                                   (remove nil?))
+                              (do (vswap! all-docs-v difference f-ds)
+                                  (mapv #(describe %) f-ds))))))))]
+    (let [ds (->> path->sdn
+                  (pmap #(->> %
+                              (apply re-root-sdn root-dir)
+                              (fix-relative-links root-dir (first %))))
+                  set
+                  (vreset! all-docs-v))
+          root-node (some->> (cfg/layers-org-query)
+                             seq
+                             (hash-map "layer")
+                             (walk ds)
+                             :children
+                             seq
+                             (apply n/root "Configuration layers" #{}))]
+      root-node
+      #_ (if (and (seq ds) root-node)
+           (update)))))
