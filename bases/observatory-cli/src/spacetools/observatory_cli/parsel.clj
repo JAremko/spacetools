597ed1d0e281d35ccdcfd8e0314ce2c2cd8b1a3b
JAremko <w3techplayground@gmail.com>
2020-09-27 09:54:18 +0300

    group up all idents

--- bases/observatory-cli/src/spacetools/observatory_cli/parsel.clj
+++ bases/observatory-cli/src/spacetools/observatory_cli/parsel.clj
@@ -1,41 +1,45 @@
 (ns spacetools.observatory-cli.parsel
-  "Elisp parser. Kinda really slow one. I made sure that there is no
-  ambiguity and look back/ahead are at minimum but it still pretty slow.
-  Howe slow? Well, it rougtly parses 1k of normal elisp lines per second.
+  "Elisp parser. Kinda slow one. I ensured that there is no ambiguity,
+  look back/ahead and recursive rules are at minimum but it still pretty slow.
+  Howe slow? Well, it parses 1k of normal Elisp lines per ~700ms.
   Yeah, it is pretty slow. Good thing that time seems to be linear.
 
-  How can I make it go faster while still using instaparse:
-  Basically deferring stuff - I can parse symbols keywords and numbers
-  as the same \"thing\" - ident. It reduces the time by something like 30%.
-  This is not that bad since I usually only need value of a ident, not type.
-  Even better time saving (around 50%) can be achieved by avoiding
-  expression parsing. It can be fine in most cases since quoted stuff is a
-  value of something and usually I look for something specific and only
-  then want to check its value. The only problem is that it will clutter
-  the code and I don't think that making parser even 4 times faster
-  radically changes anything.
+  Current trade-offs for better performance:
+  I ended up deferring some work - symbols, keywords and numbers parsed
+  as the same \"thing\" - ident-g (ident group). It reduces the time by ~30%.
+  This is not that bad since I usually only need value of an ident, not type.
 
-  Maybe I should try replacing it with something similar like
+  Malformed code like \"2'2\" throws parser error instead of being
+  treated as \"2 '2\"(Emacs does this) - It's ok because we want to detect
+  such code. But in other cases it can be interpreted differently instead of
+  throwing. For example, Emacs parses \"#b0.0\" as 2 numbers \"#b0\" and
+  \".0\" while the parser will see it as a single ident-g \"#b0.0\". I haven't
+  seen such code \"in the wild\" so it's hard to tell if this is a problem for
+  the use cases of the parser or not.
+
+  How can I make it go faster while still using instaparse:
+  40%+ boost can be achieved by avoiding \"expression\" (quotes) parsing.
+  It can be fine since quoted things usually are value and in the most cases
+  I look for something specific and only then want to check its value.
+
+  Maybe I should try replacing the whole thing with something similar, like
   https://github.com/pest-parser/pest since it is \"native\" but the lib
-  seems to struggle with recursive rules as well and I might have to
+  seems to struggle with recursive rules as well. And I might have to
   implement line number metadata generator for it. Also I doubt it will
   be faster overall. I will have to output parsed .el as EDN string and
   re-read it into the rest of my stuff. it should add noticeable overhead.
-  Overall, I would call something like 10X time reduction a win but I doubt
-  it can be achieved using this family of parsing strategies. Unless I did
-  something extremely stupid.
 
   Does speed even matter? It does when I have to parse a lot of .el files
-  in PR job. For example, to detect keybinding collisions.
+  in PR jobs. For example, to detect keybinding collisions.
   Workaround can be keeping cache of keybindings or parsed files.
   But passing something between CI jobs is a big pain in the rear end.
   Especially since I'll have to generate it on branch updates and access
   in PRs. Figuring out where to store it makes things even more difficult.
 
   Additionally compiling parser into a native-image makes it 3X times slower
-  so I'll have to run it via jdk. Good thing TravisCI has runners with it
-  pre-installed and I don't use this CI in PR jobs so I can run it totally
-  independent from the rest of the CIs."
+  so I'll have to run it via JDK. Good thing TravisCI has runners with it
+  pre-installed and I don't currently use this CI in PR jobs so I can run it
+  concurrently with the rest of the CIs."
   (:require [clojure.string :as str]
             [instaparse.combinators :as c]
             [instaparse.core :as insta]
@@ -48,8 +52,8 @@
 
 (def any-s
   "Any element."
-  "<any> = sexp | keyword | number | symbol | expr | string | vector |
-           comment | whitespace | Epsilon")
+  "<any> = sexp | ident-g | expr | string | vector | comment | whitespace |
+           Epsilon")
 
 (def comment-s
   "Comment line."
@@ -72,7 +76,7 @@
   "Expression (various quotes)."
   "<expr>   = quote | template | spread | hole
 
-   <qtbl> = sexp | keyword | number | symbol | expr | vector
+   <qtbl> = sexp | ident-g | expr | vector
 
    quote    = quote-tok qtbl
    template = tmpl-tok qtbl
@@ -81,8 +85,7 @@
 
 (def tokens-s
   "Markers of elements."
-  "<end-tok>     = sexp-r-tok | vec-r-tok | str-r-tok | quote-tok | tmpl-tok |
-                   hole-tok | spread-tok | whitespace | comment | #'$'
+  "<end-tok>     = sexp-r-tok | vec-r-tok | whitespace | comment | #'$'
 
    <sexp-l-tok>  = <'('>
    <sexp-r-tok>  = <')'>
@@ -116,14 +119,16 @@
 
    keyword   = kv-tok ident
 
-   symbol    = ! ( number | kv-tok | comment-tok | num-b-x-tok ) ident")
+   symbol    = ! ( number | kv-tok | comment-tok | num-b-x-tok ) ident
+
+   ident-g =  ident & end-tok")
 
 (def ident
   "Ident."
   {:ident
    (let [esc-ch (str/join ["\\[" "\\]" "\\(" "\\)" "\"" "\\s" "'" "," "`" ";"])
          tmpl "(?:(?:\\\\[{{ec}}])|[^{{ec}}])+"]
-     (c/hide-tag (c/regexp  (str/replace tmpl "{{ec}}" esc-ch))))})
+     (->> esc-ch (str/replace tmpl "{{ec}}") c/regexp c/hide-tag))})
 
 ;;; Parser:
 (insta/defparser ^{:doc "raw parser. Considered private."} elisp-parser
@@ -145,27 +150,30 @@
              (assoc {:tag :quote} :fn? (= f "#") :value (or s f)))}
    (insta/add-line-and-column-info-to-metadata s (elisp-parser s))))
 
-#_ (do (def text
-      "Test text"
-      ";; (1 2 3) foo
-`,@foo #'baz
-(1.0)
-(defvar configuration-layer--refresh-package-timeout dotspacemacs-elpa-timeout
-  \"Timeout in seconds to reach a package archive page.\")
-,bar
-:zzz
-\"fff\"
-     ((file-exists-p layer-dir)
-      (configuration-layer/message
-       (concat \"Cannot create configuration layer \\\"\\\", \"
-               \"this layer already exists.\") name))
-1011;; baz
-   ;; Note:
-(1+
-;; bar
-)
-(+1.0 +2 .0 0.0.0 #24r5 #b0.0 #b111 '() 2+2 2'2 +1.2b [])
-              (let ((a [1 2 3])) a)
-;")
 
-    (elisp-str->edn text))
+;; (def text
+;;   "Test text"
+;;   ";; (1 2 3) foo
+;; `,@foo #'baz
+;; (1.0)
+;; (defvar configuration-layer--refresh-package-timeout dotspacemacs-elpa-timeout
+;;   \"Timeout in seconds to reach a package archive page.\")
+;; ,bar
+;; :zzz
+;; \"fff\"
+;;      ((file-exists-p layer-dir)
+;;       (configuration-layer/message
+;;        (concat \"Cannot create configuration layer \\\"\\\", \"
+;;                \"this layer already exists.\") name))
+;; 1011;; baz
+;;    ;; Note:
+;; (1+
+;; ;; bar
+;; )
+;; (+1.0 +2 .0 0.0.0 #24r5 #b0.0 #b111 '() 2+2 2 '2 +1.2b [])
+;;               (let ((a [1 2 3])) a)
+;; ;")
+
+;; (elisp-str->edn text)
+
+;; (insta/parses elisp-parser text)
--- bases/observatory-cli/src/spacetools/observatory_cli/run.clj
+++ bases/observatory-cli/src/spacetools/observatory_cli/run.clj
@@ -1,9 +1,11 @@
 (ns spacetools.observatory-cli.run
   (:gen-class)
-  (:require [spacetools.observatory-cli.parsel :refer [elisp-str->edn]]))
+  (:require [instaparse.core :as insta]
+            [spacetools.observatory-cli.parsel :as p :refer [elisp-str->edn]]))
 
 (defn -main [file & args]
   (let [foo (slurp file)]
+    (prn (count (insta/parses p/elisp-parser foo)))
     (time (elisp-str->edn foo))
     (time (elisp-str->edn foo))
     (time (elisp-str->edn foo))
