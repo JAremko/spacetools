54cedc3197a2770903d53b027ccc6247afa0b461
JAremko <w3techplayground@gmail.com>
2020-09-27 04:59:42 +0300

    remove wrong test file

--- bases/observatory-cli/test/spacetools/observatory_cli/core_test.clj
+++
@@ -1,9 +0,0 @@
-(ns spacetools.observatory-cli.core-test
-  (:require [clojure.test :refer :all]
-            [spacetools.observatory-cli.core :as core]))
-
-;; Add tests here...
-(deftest hello-world-example-test
-  (let [output (with-out-str (core/-main))]
-    (is (= "Hello world!\n"
-           output))))
